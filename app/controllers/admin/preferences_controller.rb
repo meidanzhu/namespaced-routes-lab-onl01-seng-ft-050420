b2f8f55b4c838246b8465e606f2b7bf7b9407622
Mei Dan Zhu <meidan.zhu@gmail.com>
2020-07-23 18:08:38 +0000

    Automatically backed up by Learn

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,5 +1,12 @@
 class Admin::PreferencesController < ApplicationController
+<<<<<<< HEAD
     def index
     
     end
 end
+=======
+  def index
+
+  end
+end
+>>>>>>> d062610b787d693365c5f5c9f81568b7d5cf038c
